d49fec9ac0006ccfd20303abcbfbf28f6aacbc20
patricksu <pengsu9@gmail.com>
2020-01-31 23:46:28 -0800

    changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Feature branch.
+          Again Edit <code>src/App.js</code> and save to reload. Feature branch.
         </p>
         <a
           className="App-link"
